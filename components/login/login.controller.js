a33b7380c5b534c8f69d09b85934a2bde5ded246
Pilar <pgranadosa@ucenfotec.ac.cr>
2017-06-14 13:21:51 -0600

    Controladores y Vistas del APP

---
+++ components/login/login.controller.js
@@ -0,0 +1,29 @@
+﻿(function(){
+    'use strict';
+    angular
+    .module('app')
+    .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
+
+    function LoginController($location, AuthenticationService, FlashService){
+        var vm = this;
+        //Funcion que valida los datos de inicio de sesion y setea los estados
+        vm.login = function(){
+          vm.dataLoading = true;
+            AuthenticationService.Login(vm.username, vm.password, function(response){
+              if (response.success) {
+                  AuthenticationService.SetCredentials(vm.username, vm.password);
+                  $location.path('/');
+              }else{
+                 FlashService.Error(response.message);
+                 vm.dataLoading = false;
+             }
+            });
+        };
+        //Destruye los datos del usuario logeado, al cerrar sesion
+        (function initController() {
+            AuthenticationService.ClearCredentials();
+        })();
+    }
+})();
