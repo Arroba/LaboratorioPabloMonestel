2569c1ba5cae8d08ef4b7baa14ed167c96ab4735
wilken92 <wchacong@ucenfotec.ac.cr>
2017-06-16 11:26:44 -0600

    Habilitaciones
    volvi a habilitar los view del register, y deshabilite el rootsScope qie estaba demas

--- index.route.js
+++ index.route.js
@@ -21,12 +21,12 @@
         controller: 'HomeController',
         controllerAs: 'vm'
       })
-      // .state('registro', {
-      //   url : '/register',
-      //   templateUrl: './components/register/register.view.html',
-      //   controller: 'RegisterController',
-      //   controllerAs: 'vm'
-      // })
+         .state('registro', {
+         url : '/register',
+         templateUrl: './components/register/register.view.html',
+         controller: 'RegisterController',
+         controllerAs: 'vm'
+       })
       .state('solicitud',{
         url : '/solicitud', //ruta del url del estado
         templateUrl : './components/citas/cita.view.html',//vista que se va a cargar para este estado
@@ -49,13 +49,13 @@
             $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
         }
 
-           $rootScope.$on('$locationChangeStart', function (event, next, current) {
-               // redirect to login page if not logged in and trying to access a restricted page
-               var restrictedPage = $.inArray($location.path(), ['/iniciarSesion', '/register']) === -1;
-               var loggedIn = $rootScope.globals.currentUser;
-               if (restrictedPage && !loggedIn) {
-                   $location.path('/iniciarSesion');
-               }
-           });
+         //  $rootScope.$on('$locationChangeStart', function (event, next, current) {
+         //      // redirect to login page if not logged in and trying to access a restricted page
+         //      var restrictedPage = $.inArray($location.path(), ['/iniciarSesion', '/register']) === -1;
+         //      var loggedIn = $rootScope.globals.currentUser;
+         //      if (restrictedPage && !loggedIn) {
+        //         $location.path('/iniciarSesion');
+        //       }
+        //   });
     }
 })();
