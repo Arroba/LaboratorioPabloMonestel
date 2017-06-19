72dbab10db5489f3723df91cf0f759ee920cb8e5
wilken92 <wchacong@ucenfotec.ac.cr>
2017-06-19 17:43:05 -0600

    changes

--- components/citas/cita.service.js
+++ components/citas/cita.service.js
@@ -8,7 +8,7 @@
     var publicAPI = {
       setCitas : _setCitas,
       getCitas : _getCitas,
-      updateCita : _updateCita
+      updateState: _updateState
     };
     return publicAPI;
 
@@ -16,6 +16,10 @@
       var listaUsuarios = _getCitas();
       listaUsuarios.push(pCita);
       localStorage.setItem('listaUsuarios', JSON.stringify(listaUsuarios));
+    }
+    function _updateState(pListaCitas){
+
+      localStorage.setItem('listaUsuarios', JSON.stringify(pListaCitas));
     }
 
     function _getCitas(){
@@ -25,17 +29,7 @@
       }
       return listaUsuarios;
     }
-    function _updateCita(pObjUsuario){
-      submitActualizar.classList.add('display');
-      submit.classList.remove('display');
-      var listaUsuarios = _getCitas();
-      for (var i = 0; i < listaUsuarios.length; i++) {
-        if (listaUsuarios[i].id == pObjUsuario.id ) {
-          listaUsuarios[i] = pObjUsuario;
-        }
-      }
-      localStorage.setItem('listaUsuarios', JSON.stringify(listaUsuarios));
-    }
+
 
   }
 
