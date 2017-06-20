8ff93b2f7f7070b6c4aa42a2b683f01011942d2b
wilken92 <wchacong@ucenfotec.ac.cr>
2017-06-19 19:00:43 -0600

    correcion ruta administador
    @osantamariaacosta

--- components/login/login.controller.js
+++ components/login/login.controller.js
@@ -16,7 +16,7 @@
             AuthenticationService.Login(vm.username, vm.password, function(response){
               if (response.success) {
                   AuthenticationService.SetCredentials(vm.username, vm.password);
-                  $location.path('/admin');
+                  $location.path('/administrador');
               }else{
                  FlashService.Error(response.message);
                  vm.dataLoading = false;
