6e0288aaa5a9bd0e9ba630b1a34c850804a2392e
Pilar <pgranadosa@ucenfotec.ac.cr>
2017-06-21 09:00:37 -0600

    Espaciado
    Administrador service

--- components/services/administrador.service.js
+++ components/services/administrador.service.js
@@ -13,7 +13,6 @@
     };
     return publicAPI; // todas las funciones que sean llamadas por ajax deben estar debajo del return, para que cuando angular corra el script haga el return y devuelva el api , las funciones debajo del return son privadas y se devuelve el api que es el que contiene las funciones
 
-
     function _setUsers(pUser){
       var usersList = _getUsers();
 
@@ -37,8 +36,5 @@
       }
       localStorage.setItem('lsUsersList', JSON.stringify(usersList));
     }
-
-
   }
-
 })();
