eefcb4e42facba6621d89ca74ca886f4d8032801
wilken92 <wchacong@ucenfotec.ac.cr>
2017-06-21 08:20:02 -0600

    archivos para master

---
+++ components/citas/cita.controller.js
@@ -0,0 +1,76 @@
+(function(){
+  'use strict';
+  angular
+    .module('gymApp')
+    .controller('citaController', citaController);
+    function citaController(citaService){
+
+      var citaCtrl = this;
+
+      function init(){
+        citaCtrl.citas = citaService.getCitas();
+
+      }init();
+
+      citaCtrl.save= function(){
+
+        var newCita = {
+          id: citaCtrl.id,
+          descripcion: citaCtrl.descripcion,
+          fecha: citaCtrl.fecha,
+          hora: citaCtrl.hora,
+          estado: 'En Revisión'
+        }
+
+        citaService.setCitas(newCita);
+        limpiar();
+        init();
+
+      };
+
+
+      citaCtrl.getInfo = function(pcita){
+        submitActualizar.classList.remove('display');
+        submit.classList.add('display');
+        citaCtrl.id = pcita.id;
+        citaCtrl.descripcion = pcita.descripcion;
+        citaCtrl.fecha = pcita.fecha;
+        citaCtrl.hora = pcita.hora;
+      }
+
+      function limpiar(){
+        citaCtrl.id = "";
+        citaCtrl.descripcion = "";
+        citaCtrl.fecha = "";
+        citaCtrl.hora = "";
+      }
+      citaCtrl.aprobacion = function(cita){
+        var listaUsuarios = citaService.getCitas();
+          for (var i = 0; i < listaUsuarios.length; i++) {
+            if (listaUsuarios[i].id == cita.id) {
+              listaUsuarios[i].estado = 'Aprobada';
+              console.log(listaUsuarios[i].estado)
+            }// Cierre del if
+          }// Cierre del ciclo
+        citaService.updateState(listaUsuarios);
+        init();
+      }// Cierre de la funcion aprobación
+
+      citaCtrl.remover = function(cita){
+        var listaUsuarios = citaService.getCitas();
+        for (var i = 0; i < listaUsuarios.length; i++) {
+          if (listaUsuarios[i].id == cita.id) {
+            listaUsuarios[i].estado = 'Rechazada';
+            console.log(listaUsuarios[i].estado)
+          }// Cierre del if
+        }// Cierre del ciclo
+      citaService.updateState(listaUsuarios);
+      init();
+      }// Cierre de la funcion aprobación
+
+
+
+    }
+
+
+})();
