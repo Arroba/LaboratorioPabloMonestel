2221a2595c9412ebbfe18bf5d1e42efa6ce33066
FabianQF <fquirosf@ucenfotec.ac.cr>
2017-06-21 09:38:41 -0600

    Finalización de comentarios calculoService
    Continución de comentarios

--- components/services/calculo.service.js
+++ components/services/calculo.service.js
@@ -32,46 +32,46 @@
       return usersList;
     }
     // Fin de función de obtener datos actualizados
-    
 
-    //Función de permitir cambios editados, si, solo si la identificación es la misma
-    function _updateUser(pobjUsuario){
-      var usersList = _getUsers();
-      for(var i = 0; i < usersList.length; i++){
-        if(usersList[i].id == pobjUsuario.id){
-          usersList[i] = pobjUsuario;
-        }
+
+    //Inicio de función de permitir cambios editados, si, solo si la identificación es la misma
+  function _updateUser(pobjUsuario){
+    var usersList = _getUsers();
+    for(var i = 0; i < usersList.length; i++){
+      if(usersList[i].id == pobjUsuario.id){
+        usersList[i] = pobjUsuario;
       }
-      localStorage.setItem('lsUsersList', JSON.stringify(usersList));
     }
+    localStorage.setItem('lsUsersList', JSON.stringify(usersList));
+  }
+  //Fin de función de permitir cambios editados, si, solo si la identificación es la misma
 
-
-  //   function _porcentajeGrasaSubCutanea(){
-  //     var usersList = _getUsers();
-  //     var nSumatoriaPliegues = 0;
-  //     var nPorcentajeGrasa = 0;
-  //     var nConstanteMasculina = 0.1051;
-  //     var nConstanteFemenina = 0.1548;
-  //
-  //     for(var i = 0; i < usersList.length; i++){
-  //
-  //       var nSumatoriaPliegues = usersList[i].tricepIzq + usersList[i].tricepDer + usersList[i].subescapularDer + usersList[i].subescapularIzq + usersList[i].supraespinalDer + usersList[i].supraespinalIzq +usersList[i].abdominalDer + usersList[i].abdominalIzq + usersList[i].musloDerPliegue + usersList[i].musloIzqPliegue + usersList[i].pantorrillaIzqPliegue + usersList[i].pantorrillaDerPliegue;
-  //
-  //       if (usersList[i].genero == masculino || indefinido) {
-  //         nPorcentajeGrasa = nConstanteMasculina * nSumatoriaPliegues + (2.585);
-  //         nPesoGraso = ( usersList[i].peso  * nPorcentajeGrasa) / 100; // falta Peso
-  //         nPorcentajeMasa = usersList[i].peso - nPesoGraso;
-  //       }else{
-  //         if (usersList[i].genero == femenino) {
-  //           nPorcentajeGrasa = nConstanteFemenina * nSumatoriaPliegues + (3.580);
-  //           nPesoGraso = ( usersList[i].peso  * nPorcentajeGrasa) / 100; // falta Peso
-  //           nPorcentajeMasa = usersList[i].peso - nPesoGraso;
-  //       }
-  //     }
-  //   }
-  //   return nPorcentajeGrasa;
-  //   localStorage.setItem('lsUsersList', JSON.stringify(usersList));
-  // }
+//   function _porcentajeGrasaSubCutanea(){
+//     var usersList = _getUsers();
+//     var nSumatoriaPliegues = 0;
+//     var nPorcentajeGrasa = 0;
+//     var nConstanteMasculina = 0.1051;
+//     var nConstanteFemenina = 0.1548;
+//
+//     for(var i = 0; i < usersList.length; i++){
+//
+//       var nSumatoriaPliegues = usersList[i].tricepIzq + usersList[i].tricepDer + usersList[i].subescapularDer + usersList[i].subescapularIzq + usersList[i].supraespinalDer + usersList[i].supraespinalIzq +usersList[i].abdominalDer + usersList[i].abdominalIzq + usersList[i].musloDerPliegue + usersList[i].musloIzqPliegue + usersList[i].pantorrillaIzqPliegue + usersList[i].pantorrillaDerPliegue;
+//
+//       if (usersList[i].genero == masculino || indefinido) {
+//         nPorcentajeGrasa = nConstanteMasculina * nSumatoriaPliegues + (2.585);
+//         nPesoGraso = ( usersList[i].peso  * nPorcentajeGrasa) / 100; // falta Peso
+//         nPorcentajeMasa = usersList[i].peso - nPesoGraso;
+//       }else{
+//         if (usersList[i].genero == femenino) {
+//           nPorcentajeGrasa = nConstanteFemenina * nSumatoriaPliegues + (3.580);
+//           nPesoGraso = ( usersList[i].peso  * nPorcentajeGrasa) / 100; // falta Peso
+//           nPorcentajeMasa = usersList[i].peso - nPesoGraso;
+//       }
+//     }
+//   }
+//   return nPorcentajeGrasa;
+//   localStorage.setItem('lsUsersList', JSON.stringify(usersList));
+// }
 
 }
 })();
